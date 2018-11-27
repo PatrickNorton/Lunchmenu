18281e0bec2ffae8164e267eea3119de566133b9
PatrickNorton <patrick.147.norton@gmail.com>
2018-11-27 10:23:39 -0500

    Added change in menu for after lunch

--- Menureader.py
+++ Menureader.py
@@ -2,11 +2,16 @@ import os
 from datetime import datetime
 currdir = os.getcwd()
 monthname = datetime.now().strftime('%B')
+currday = datetime.now().day
 currtime = datetime.now().hour
 filenm = monthname+'.txt'
 thefile = os.path.join(currdir, filenm)
 try:
     with open(thefile) as f:
         text = f.readlines()
+        if currtime > 13:
+            pass  # Read tomorrow's menu
+        else:
+            pass  # Read today's menu
 except FileNotFoundError:
     print("This month's file is not yet on here!")
